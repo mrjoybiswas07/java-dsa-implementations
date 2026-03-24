0b427182552e590e28f8afbd9e6b44e250edcef6
mrjoybiswas07 <mrjoybiswas1@gmail.com>
2026-03-24 09:51:02 +0530

    climbing stairs java code using memoization

---
+++ Dynamic_Programming/Climbing_Stairs_Memoization.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class Climbing_Stairs_Memoization { // (n)
+    public static int countWays(int n, int ways[]) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+        if (ways[n] != -1) { // already clculated
+            return ways[n];
+
+        }
+        ways[n] = countWays(n - 1, ways) + countWays(n - 2, ways);
+        return ways[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int ways[] = new int[n + 1]; // 0 0 0 0 0
+        Arrays.fill(ways, -1); // -1 -1 -1 -1
+        System.out.println(countWays(n, ways));
+    }
+
+}
