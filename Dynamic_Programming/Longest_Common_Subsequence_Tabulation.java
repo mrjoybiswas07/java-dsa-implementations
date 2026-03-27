17005eb63ba0944cbb344d4112ec448121f3891a
mrjoybiswas07 <mrjoybiswas1@gmail.com>
2026-03-27 15:12:43 +0530

    modify longest common subsequence tabulation problem

--- Dynamic_Programming/Longest_Common_Subsequence_Tabulation.java
+++ Dynamic_Programming/Longest_Common_Subsequence_Tabulation.java
@@ -1,11 +1,9 @@
 public class Longest_Common_Subsequence_Tabulation {
-
     public static int lcsTab(String str1, String str2) {
         int n = str1.length();
         int m = str2.length();
 
         int dp[][] = new int[n + 1][m + 1];
-
         for (int i = 0; i < n + 1; i++) {
             for (int j = 0; j < m + 1; j++) {
                 if (i == 0 || j == 0) {
@@ -14,26 +12,26 @@ public class Longest_Common_Subsequence_Tabulation {
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
 
-                // if characters match
+                 
                 if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    // take max from top or left
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(ans1, ans2);
                 }
+
             }
         }
-
-        return dp[n][m]; // final answer
+        return dp[n][m];
     }
 
     public static void main(String[] args) {
-        String str1 = "abcdge";
-        String str2 = "abedg"; // lcs = "abdg"; length
-
-        System.out.println(lcsTab(str1, str2)); // Output: 3 ("abd")
+        String str1 = "abcde";
+        String str2 = "abed";
+        System.out.println(lcsTab(str1, str2));
     }
 }
