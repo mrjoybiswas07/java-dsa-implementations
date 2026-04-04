414a2e400e036365d377c9ea3284e7ed8ee27da9
mrjoybiswas07 <mrjoybiswas1@gmail.com>
2026-04-04 19:25:52 +0530

    updated code catalan number memeoization

---
+++ Dynamic_Programming/Catalan_Number_Memoization.java
@@ -0,0 +1,29 @@
+public class Catalan_Number_Memoization {
+    public static int catalanMemo(int n, int dp[]) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        int ans = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            ans += catalanMemo(i, dp) * catalanMemo(n - i - 1, dp);
+        }
+
+        return dp[n] = ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int dp[] = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) { // Arrays.fill(dp,-1)
+            dp[i] = -1; 
+        }
+
+        System.out.println(catalanMemo(n, dp)); // 14
+    }
+}
